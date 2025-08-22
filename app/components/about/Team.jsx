edfc1671e146bcbd1639570b32491685df35841c
TAHSEEN-CH <m.tahseen294@gmail.com>
2025-08-22 17:22:23 +0500

    Add images & Videos

--- app/components/about/Team.jsx
+++ app/components/about/Team.jsx
@@ -92,6 +92,7 @@ export const Team = () => {
                     </p>
                 </div>
 
+
                 {/* Video grid */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12">
                     {videos.map((video, index) => (
